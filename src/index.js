99805b61270c3b89bc48e65b55883915315147ad
Quang Tung Nguyen <QuangTung.Nguyen@episerver.com>
2019-09-27 13:40:13 +0700

    init list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Table } from 'react-bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Student {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// attempt to store in global variable
+const StudentData = React.createContext([
+    // { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+    // { firstName: 'Raisa', lastName: 'Padgett', age: 17 },
+    // { firstName: 'Segars', lastName: 'Retha', age: 21 },
+    // { firstName: 'Wilbert', lastName: 'Fava', age: 20 },
+    // { firstName: 'Marleen', lastName: 'Rusch', age: 16 },
+    // { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+    // { firstName: 'Larea', lastName: 'Griffery', age: 18 },
+    // { firstName: 'Patrick', lastName: 'Escudero', age: 20 },
+    // { firstName: 'Loralle', lastName: 'Amaker', age: 22 },
+
+    new Student('Kennith', 'Rusch', 15),
+    new Student('Raisa', 'Padgett', 17),
+    new Student('Segars', 'Retha', 21),
+    new Student('Wilbert', 'Fava', 20),
+    new Student('Marleen', 'Rusch', 16),
+    new Student('Kennith', 'Rusch', 15),
+    new Student('Larea', 'Griffery', 18),
+    new Student('Patrick', 'Escudero', 20),
+    new Student('Loralle', 'Amaker', 22),
+])
+
+class StudentTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.data = [
+            new Student('Kennith', 'Rusch', 15),
+            new Student('Raisa', 'Padgett', 17),
+            new Student('Segars', 'Retha', 21),
+            new Student('Wilbert', 'Fava', 20),
+            new Student('Marleen', 'Rusch', 16),
+            new Student('Kennith', 'Rusch', 15),
+            new Student('Larea', 'Griffery', 18),
+            new Student('Patrick', 'Escudero', 20),
+            new Student('Loralle', 'Amaker', 22),
+        ];
+    }
+    render() {
+        return (
+            <Table hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.data.map((value, index) => {
+                        return (
+                            <tr>
+                                <td>{index}</td>
+                                <td>{value.firstName}</td>
+                                <td>{value.lastName}</td>
+                                <td>{value.age}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </Table>
+        );
+    }
+}
+
+ReactDOM.render(
+    <StudentTable />,
+    document.getElementById('root')
+)
