f62c6c5e6b77dce3611491e3cd25e902395d856e
Ichisada Shioko <ichisadashioko@gmail.com>
2019-09-27 14:00:07 +0700

    store data in local storage

--- src/index.js
+++ src/index.js
@@ -4,51 +4,53 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Table } from 'react-bootstrap';
 
-class Student {
-    constructor(firstName, lastName, age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
+// class Student {
+//     constructor(firstName, lastName, age) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//     }
+// }
+
+// new Student('Kennith', 'Rusch', 15),
+// new Student('Raisa', 'Padgett', 17),
+// new Student('Segars', 'Retha', 21),
+// new Student('Wilbert', 'Fava', 20),
+// new Student('Marleen', 'Rusch', 16),
+// new Student('Kennith', 'Rusch', 15),
+// new Student('Larea', 'Griffery', 18),
+// new Student('Patrick', 'Escudero', 20),
+// new Student('Loralle', 'Amaker', 22),
+// { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+// { firstName: 'Raisa', lastName: 'Padgett', age: 17 },
+// { firstName: 'Segars', lastName: 'Retha', age: 21 },
+// { firstName: 'Wilbert', lastName: 'Fava', age: 20 },
+// { firstName: 'Marleen', lastName: 'Rusch', age: 16 },
+// { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+// { firstName: 'Larea', lastName: 'Griffery', age: 18 },
+// { firstName: 'Patrick', lastName: 'Escudero', age: 20 },
+// { firstName: 'Loralle', lastName: 'Amaker', age: 22 },
+
+if (!localStorage.getItem('studentData')) {
+    localStorage.setItem('studentData', JSON.stringify([
+        { firstName: 'Patrick', lastName: 'Escudero', age: 20 },
+        { firstName: 'Loralle', lastName: 'Amaker', age: 22 },
+        { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+        { firstName: 'Raisa', lastName: 'Padgett', age: 17 },
+        { firstName: 'Segars', lastName: 'Retha', age: 21 },
+        { firstName: 'Wilbert', lastName: 'Fava', age: 20 },
+        { firstName: 'Marleen', lastName: 'Rusch', age: 16 },
+        { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
+        { firstName: 'Larea', lastName: 'Griffery', age: 18 },
+    ]));
 }
-
-// attempt to store in global variable
-const StudentData = React.createContext([
-    // { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
-    // { firstName: 'Raisa', lastName: 'Padgett', age: 17 },
-    // { firstName: 'Segars', lastName: 'Retha', age: 21 },
-    // { firstName: 'Wilbert', lastName: 'Fava', age: 20 },
-    // { firstName: 'Marleen', lastName: 'Rusch', age: 16 },
-    // { firstName: 'Kennith', lastName: 'Rusch', age: 15 },
-    // { firstName: 'Larea', lastName: 'Griffery', age: 18 },
-    // { firstName: 'Patrick', lastName: 'Escudero', age: 20 },
-    // { firstName: 'Loralle', lastName: 'Amaker', age: 22 },
-
-    new Student('Kennith', 'Rusch', 15),
-    new Student('Raisa', 'Padgett', 17),
-    new Student('Segars', 'Retha', 21),
-    new Student('Wilbert', 'Fava', 20),
-    new Student('Marleen', 'Rusch', 16),
-    new Student('Kennith', 'Rusch', 15),
-    new Student('Larea', 'Griffery', 18),
-    new Student('Patrick', 'Escudero', 20),
-    new Student('Loralle', 'Amaker', 22),
-])
 
 class StudentTable extends React.Component {
     constructor(props) {
         super(props);
-        this.data = [
-            new Student('Kennith', 'Rusch', 15),
-            new Student('Raisa', 'Padgett', 17),
-            new Student('Segars', 'Retha', 21),
-            new Student('Wilbert', 'Fava', 20),
-            new Student('Marleen', 'Rusch', 16),
-            new Student('Kennith', 'Rusch', 15),
-            new Student('Larea', 'Griffery', 18),
-            new Student('Patrick', 'Escudero', 20),
-            new Student('Loralle', 'Amaker', 22),
-        ];
+        this.data = JSON.parse(localStorage.getItem('studentData'));
+        console.log(typeof this.data)
+        console.log(this.data)
     }
     render() {
         return (
